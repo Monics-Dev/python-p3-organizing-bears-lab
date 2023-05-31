e3e8df5e09f7e04c1cde0d260543ee6987026436
Monica <monijerobon@gmail.com>
2023-05-31 14:32:55 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("MrChocolate",10 ,"M","Black",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("Rowdy",8 ,"M","White",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("Tabitha",12 ,"F","Black",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("Sergeant",18 ,"M","Brown",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("Melisa",8 ,"F","Black",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("Grinch",16 ,"M","Brown",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES("Wendy",9 ,"F","Black",NULL,"True");
+INSERT INTO bears(name ,age ,sex,color,temperament, alive)
+VALUES(NULL,NULL,NULL,NULL,NULL,"False");
+
+
+
